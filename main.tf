1a1dc4ead5cacd27520c4f50d599094e62b706d2
Nodir Abdugafforov <nodirabdugafforov@Nodirs-MacBook-Pro.local>
2023-03-10 18:55:39 -0500

    File have been modified

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "public_subnet" {
 }
 
 ## Private Subnet 1
-resource "aws_subnet" "private_subnet_first" {
+resource "aws_subnet" "private_subnet_1" {
   vpc_id = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = data.aws_availability_zone.us-east-1a.id
@@ -34,7 +34,7 @@ resource "aws_subnet" "private_subnet_first" {
 }
 
 ## Private Subnet 2
-resource "aws_subnet" "private_subnet_second" {
+resource "aws_subnet" "private_subnet_2" {
   vpc_id = aws_vpc.my_vpc.id
   cidr_block = "10.0.2.0/24"
   availability_zone = data.aws_availability_zone.us-east-1a.id
@@ -45,7 +45,7 @@ resource "aws_subnet" "private_subnet_second" {
 }
 
 ## Private Subnet 3
-resource "aws_subnet" "private_subnet_third" {
+resource "aws_subnet" "private_subnet_3" {
   vpc_id = aws_vpc.my_vpc.id
   cidr_block = "10.0.3.0/24"
   availability_zone = data.aws_availability_zone.us-east-1a.id
@@ -84,7 +84,7 @@ data "aws_availability_zone" "us-east-1b" {
 }
 
 ## Public Subnet
-resource "aws_subnet" "public_st" {
+resource "aws_subnet" "public_subneet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.0.0/24"
   availability_zone = data.aws_availability_zone.us-east-1b.id
@@ -95,7 +95,7 @@ resource "aws_subnet" "public_st" {
 }
 
 ## Private Subnet 1
-resource "aws_subnet" "private_st1" {
+resource "aws_subnet" "private_subnet_first" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = data.aws_availability_zone.us-east-1b.id
@@ -106,7 +106,7 @@ resource "aws_subnet" "private_st1" {
 }
 
 ## Private Subnet 2
-resource "aws_subnet" "private_st2" {
+resource "aws_subnet" "private_subnet_second" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.2.0/24"
   availability_zone = data.aws_availability_zone.us-east-1b.id
@@ -117,7 +117,7 @@ resource "aws_subnet" "private_st2" {
 }
 
 ## Private subnet 3
-resource "aws_subnet" "private_st3" {
+resource "aws_subnet" "private_subnet_third" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.3.0/24"
   availability_zone = data.aws_availability_zone.us-east-1b.id
