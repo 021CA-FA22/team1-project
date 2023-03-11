9e3701c9f06ac13770239e8fc9286a522f9c2def
Nodir Abdugafforov <nodirabdugafforov@Nodirs-MacBook-Pro.local>
2023-03-10 19:15:26 -0500

    IAM/variables.tf and main.tf have beeeen modified

--- IAM/variables.tf
+++ IAM/variables.tf
@@ -1,14 +1,14 @@
 variable "dbadmin_users" {
-  type = list(any)
-  default = [ "dbadmin1", "dbadmin2" ]
+  type    = list(any)
+  default = ["dbadmin1", "dbadmin2"]
 }
 
 variable "monitor_users" {
-  type = list(any)
-  default = [ "monitoruser1", "monitoruser2", "monitoruser3", "monitoruser4" ]
+  type    = list(any)
+  default = ["monitoruser1", "monitoruser2", "monitoruser3", "monitoruser4"]
 }
 
 variable "sysadmin_users" {
-  type = list(any)
-  default = [ "sysadmin1", "sysadmin2" ]
+  type    = list(any)
+  default = ["sysadmin1", "sysadmin2"]
 }
--- main.tf
+++ main.tf
@@ -13,9 +13,9 @@ data "aws_availability_zone" "us-east-1a" {
 
 ## Public Subnet
 resource "aws_subnet" "public_subnet" {
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "10.0.0.0/24"
-  availability_zone = data.aws_availability_zone.us-east-1a.id
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = data.aws_availability_zone.us-east-1a.id
   map_public_ip_on_launch = true
   tags = {
     Name = "Public Subnet"
@@ -24,9 +24,9 @@ resource "aws_subnet" "public_subnet" {
 
 ## Private Subnet 1
 resource "aws_subnet" "private_subnet_1" {
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = data.aws_availability_zone.us-east-1a.id
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zone.us-east-1a.id
   map_public_ip_on_launch = false
   tags = {
     Name = "Private Subnet 1"
@@ -35,9 +35,9 @@ resource "aws_subnet" "private_subnet_1" {
 
 ## Private Subnet 2
 resource "aws_subnet" "private_subnet_2" {
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = data.aws_availability_zone.us-east-1a.id
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = data.aws_availability_zone.us-east-1a.id
   map_public_ip_on_launch = false
   tags = {
     Name = "Private Subnet 2"
@@ -46,9 +46,9 @@ resource "aws_subnet" "private_subnet_2" {
 
 ## Private Subnet 3
 resource "aws_subnet" "private_subnet_3" {
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "10.0.3.0/24"
-  availability_zone = data.aws_availability_zone.us-east-1a.id
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = data.aws_availability_zone.us-east-1a.id
   map_public_ip_on_launch = false
   tags = {
     Name = "Private Subnet 3"
@@ -85,9 +85,9 @@ data "aws_availability_zone" "us-east-1b" {
 
 ## Public Subnet
 resource "aws_subnet" "public_subneet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.0.0/24"
-  availability_zone = data.aws_availability_zone.us-east-1b.id
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = data.aws_availability_zone.us-east-1b.id
   map_public_ip_on_launch = true
   tags = {
     Name = "Public Subnet"
@@ -96,9 +96,9 @@ resource "aws_subnet" "public_subneet" {
 
 ## Private Subnet 1
 resource "aws_subnet" "private_subnet_first" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = data.aws_availability_zone.us-east-1b.id
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zone.us-east-1b.id
   map_public_ip_on_launch = false
   tags = {
     Name = "Private Subnet 1"
@@ -107,9 +107,9 @@ resource "aws_subnet" "private_subnet_first" {
 
 ## Private Subnet 2
 resource "aws_subnet" "private_subnet_second" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = data.aws_availability_zone.us-east-1b.id
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = data.aws_availability_zone.us-east-1b.id
   map_public_ip_on_launch = false
   tags = {
     Name = "Private Subnet 2"
@@ -118,9 +118,9 @@ resource "aws_subnet" "private_subnet_second" {
 
 ## Private subnet 3
 resource "aws_subnet" "private_subnet_third" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.3.0/24"
-  availability_zone = data.aws_availability_zone.us-east-1b.id
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = data.aws_availability_zone.us-east-1b.id
   map_public_ip_on_launch = false
   tags = {
     Name = "Private Subnet 3"
