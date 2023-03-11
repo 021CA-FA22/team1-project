8b8d726317c2aa1071b5eaa0d29a4e23bc8a9960
Nodir Abdugafforov <nodirabdugafforov@Nodirs-MacBook-Pro.local>
2023-03-11 15:42:48 -0500

    Now Module has beeeen fixed y'all can work on it as it is

--- MODULE/child/VPC/main.tf
+++ MODULE/child/VPC/main.tf
@@ -8,11 +8,11 @@ resource "aws_vpc" "my_vpc" {
 }
 
 ####### DATA SOURCE FOR AVAILABILITY ###########
-data "aws_availability_zone" "us-east-1a" {
+data "aws_availability_zone" "first_availability_zone" {
   name = "us-east-1a"
 }
 
-data "aws_availability_zone" "us-east-1b" {
+data "aws_availability_zone" "second_availability_zone" {
   name = "us-east-1b"
 }
 
@@ -20,7 +20,7 @@ data "aws_availability_zone" "us-east-1b" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = var.public_subnet_cidr
-  availability_zone       = data.aws_availability_zone.us-east-1a.name
+  availability_zone       = data.aws_availability_zone.first_availability_zone.id
   map_public_ip_on_launch = true
   tags = {
     "Name" = "${title("${var.env_tag}")} Public Subnet"
@@ -31,7 +31,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet_1" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = var.private_subnet_cidr_1
-  availability_zone       = data.aws_availability_zone.us-east-1a.name
+  availability_zone       = data.aws_availability_zone.first_availability_zone.id
   map_public_ip_on_launch = false
   tags = {
     "Name" = "${title("${var.env_tag}")} Private Subnet 1"
@@ -42,7 +42,7 @@ resource "aws_subnet" "private_subnet_1" {
 resource "aws_subnet" "private_subnet_2" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = var.private_subnet_cidr_2
-  availability_zone       = data.aws_availability_zone.us-east-1a.name
+  availability_zone       = data.aws_availability_zone.first_availability_zone.id
   map_public_ip_on_launch = false
   tags = {
     "Name" = "${title("${var.env_tag}")} Private Subnet 2"
@@ -53,7 +53,7 @@ resource "aws_subnet" "private_subnet_2" {
 resource "aws_subnet" "private_subnet_3" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = var.private_subnet_cidr_3
-  availability_zone       = data.aws_availability_zone.us-east-1a.name
+  availability_zone       = data.aws_availability_zone.first_availability_zone.id
   map_public_ip_on_launch = false
   tags = {
     "Name" = "${title("${var.env_tag}")} Private Subnet 3"
@@ -64,7 +64,7 @@ resource "aws_subnet" "private_subnet_3" {
 resource "aws_subnet" "public_subneet" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = var.public_subneet_cidr
-  availability_zone       = data.aws_availability_zone.us-east-1b.name
+  availability_zone       = data.aws_availability_zone.second_availability_zone.id
   map_public_ip_on_launch = true
   tags = {
     "Name" = "${title("${var.env_tag}")} Public Subneet"
@@ -75,7 +75,7 @@ resource "aws_subnet" "public_subneet" {
 resource "aws_subnet" "private_subnet_first" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = var.private_subnet_cidr_first
-  availability_zone       = data.aws_availability_zone.us-east-1b.name
+  availability_zone       = data.aws_availability_zone.second_availability_zone.id
   map_public_ip_on_launch = false
   tags = {
     "Name" = "${title("${var.env_tag}")} Private Subneet 1"
@@ -86,7 +86,7 @@ resource "aws_subnet" "private_subnet_first" {
 resource "aws_subnet" "private_subnet_second" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = var.private_subnet_cidr_second
-  availability_zone       = data.aws_availability_zone.us-east-1b.name
+  availability_zone       = data.aws_availability_zone.second_availability_zone.id
   map_public_ip_on_launch = false
   tags = {
     "Name" = "${title("${var.env_tag}")} Private Subneet 2"
@@ -97,7 +97,7 @@ resource "aws_subnet" "private_subnet_second" {
 resource "aws_subnet" "private_subnet_third" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = var.private_subnet_cidr_third
-  availability_zone       = data.aws_availability_zone.us-east-1b.name
+  availability_zone       = data.aws_availability_zone.second_availability_zone.id
   map_public_ip_on_launch = false
   tags = {
     "Name" = "${title("${var.env_tag}")} Private Subneet 3"
