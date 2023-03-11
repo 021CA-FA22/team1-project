7b1d2218c254ffdc5df4f401132f606fb457f604
Nodir Abdugafforov <nodirabdugafforov@Nodirs-MacBook-Pro.local>
2023-03-11 12:16:24 -0500

    Some messages have been added to the backend.tf file just check it out

--- MODULE/root/backend.tf
+++ MODULE/root/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "395524572806"
+    bucket = "395524572806"       ## <<<<< You have to use the existing bucket on your (AWS Console) (S3) any buckets that are available or give it random numbers Terraform will be created an (S3) bucket for you!!! 
     key    = "module-tfstate"
     region = "us-east-1"
   }
